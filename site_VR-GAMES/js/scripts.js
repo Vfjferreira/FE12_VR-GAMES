0cf16df4857dc0dcee2ac2511d678cc10bcaa1ed
Vfjferreira <vfjferreira@gmail.com>
2023-01-13 20:15:29 -0300

    jq

--- site_VR-GAMES/js/scripts.js
+++ site_VR-GAMES/js/scripts.js
@@ -1,1 +1,19 @@
-alert ("js ok")
+$(document).ready(function(){
+    
+    $("#bars").click(function(){
+        if ($("#menu").hasClass("menu-ativo")){
+         $("#menu").removeClass("menu-ativo")
+        }else{
+         $("#menu").addClass("menu-ativo")
+        }
+     })
+ })
+    /*function mostrarmenu($event){
+    let menu = document.getElementById("menu")
+        if(getComputedStyle(menu).display == "none"){
+           menu.style.display = "flex"
+        }else{
+            menu.style.display = "none"
+        }   
+  event.preventdefault()
+} */
